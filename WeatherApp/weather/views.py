7c31488856fd83d0f6fc08dd4cb45560af24dbdb
User <ev9esh.mail@gmail.com>
2022-08-07 14:06:37 +0300

    add def info

--- WeatherApp/weather/views.py
+++ WeatherApp/weather/views.py
@@ -1,20 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 import requests
 from .models import City
 from .forms import CityForm
-from requests import Response
+from django.http import HttpResponse
 
 
 def index(request):
-    appid = '4c134bf1102a32bac429024a3b8cf39c'
-    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=' + appid
-
     if request.method == 'POST':
         form = CityForm(request.POST)
         if form.is_valid():
             form.save()
+            return redirect('info')
+    else:
+        form = CityForm
+        context = {
+            'form': form,
+        }
+        return render(request, 'weather/index.html', context)
 
-    form = CityForm
+
+def info(request):
+    appid = '4c134bf1102a32bac429024a3b8cf39c'
+    url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&lang=ru&appid=' + appid
 
     cities = City.objects.order_by('-id')
     all_cities = []
@@ -27,10 +34,5 @@ def index(request):
             'speed': res['wind']['speed']
         }
         all_cities.append(city_info)
-    context = {'all_info': all_cities, 'form': form}
-    return render(request, 'weather/index.html', context)
-
-
-def info(request):
-    city = City.objects.order_by('-id')
-    return render(request, 'weather/info.html', city)
+    context = {'all_info': all_cities}
+    return render(request, 'weather/info.html', context)
